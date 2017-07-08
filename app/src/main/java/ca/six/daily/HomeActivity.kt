6a1702d3e84686ca133b3bb35e13736564a8f076
songzhw <songzhw2012@gmail.com>
2017-07-07 22:05:56 -0400

    init project

---
+++ app/src/main/java/ca/six/daily/HomeActivity.kt
@@ -0,0 +1,12 @@
+package ca.six.daily
+
+import android.os.Bundle
+import ca.six.daily.core.BaseActivity
+
+class HomeActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/ca/six/daily/core/BaseActivity.kt
@@ -0,0 +1,7 @@
+package ca.six.daily.core
+
+import android.support.v7.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+
+}
