d2712a7e7d881180d3f6b0211d3bc4220f257fa0
songzhw <songzhw2012@gmail.com>
2017-07-08 11:46:34 -0400

    refactor the http call

--- app/src/main/java/ca/six/daily/biz/home/DailyListActivity.kt
+++ app/src/main/java/ca/six/daily/biz/home/DailyListActivity.kt
@@ -3,12 +3,7 @@ package ca.six.daily.biz.home
 import android.os.Bundle
 import ca.six.daily.R
 import ca.six.daily.core.BaseActivity
-import ca.six.daily.core.BaseApp
 import ca.six.daily.core.network.HttpEngine
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-
 
 // 第一屏内容(banner与list) ： https://news-at.zhihu.com/api/4/news/latest
 class DailyListActivity : BaseActivity() {
@@ -17,12 +12,7 @@ class DailyListActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_list)
 
-        Observable.create<String> { emitter ->
-                    val req = HttpEngine.request(BaseApp.http,"news/latest")
-                    emitter.onNext(req)
-                }
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+        HttpEngine.request("news/latest")
                 .subscribe{str -> println("szw rx : ret = $str")}
     }
 
--- app/src/main/java/ca/six/daily/core/network/HttpEngine.kt
+++ app/src/main/java/ca/six/daily/core/network/HttpEngine.kt
@@ -1,16 +1,30 @@
 package ca.six.daily.core.network
 
+import ca.six.daily.core.BaseApp
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import okhttp3.Request
 import okhttp3.OkHttpClient
 
 object HttpEngine {
     val PREFIX = " https://news-at.zhihu.com/api/4/"
 
-    fun request(http: OkHttpClient, end : String): String {
+    fun requestString(http: OkHttpClient, end: String): String {
         val req = Request.Builder()
                 .url(PREFIX + end)
                 .build()
         val resp = http.newCall(req).execute()
         return resp.body()?.string() ?: "" // 三目运算符
+    }
+
+    fun request(end: String): Observable<String> {
+        val observable = Observable.create<String> { emitter ->
+                    val responseString = requestString(BaseApp.http, end)
+                    emitter.onNext(responseString)
+                }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+        return observable
     }
 }
