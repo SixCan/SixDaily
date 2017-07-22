750053a133db2e87a46cb7734cb1e852b3c988e8
songzhw <songzhw2012@gmail.com>
2017-07-21 22:47:49 -0400

    update unit test

--- app/src/test/java/ca/six/daily/utils/DataUtilsTest.kt
+++ app/src/test/java/ca/six/daily/utils/DataUtilsTest.kt
@@ -1,3 +1,4 @@
+
 package ca.six.daily.utils
 
 import android.os.Build
@@ -5,6 +6,7 @@ import ca.six.daily.BuildConfig
 import ca.six.daily.core.BaseApp
 import ca.six.daily.utils.isCacheFileExist
 import ca.six.daily.utils.writeToCacheFile
+import org.junit.After
 import org.junit.Assert
 import org.junit.Test
 
@@ -15,12 +17,18 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 import org.robolectric.shadows.ShadowEnvironment
+import java.io.File
 
 @RunWith(RobolectricTestRunner::class)
 @Config(constants = BuildConfig::class, sdk = intArrayOf(Build.VERSION_CODES.LOLLIPOP)
         ,application = BaseApp::class)
 class DataUtilsTest {
-    val fileName = "temp.txt"
+    val fileName = "news_latest.json"
+
+    @Test
+    fun testIsExisting_returnFalse_whenAtFirst(){
+        assertFalse(isCacheFileExist(fileName))
+    }
 
     @Test
     fun testCacheFile_whenSuccess() {
@@ -28,6 +36,36 @@ class DataUtilsTest {
         assertTrue(isCacheFileExist(fileName))
         assertEquals("test", readCacheFile(fileName))
     }
+
+    @Test
+    fun testReadCachedLatestNews_whenNoCache_shouldObserveComplete(){
+
+    }
+
+    @Test
+    fun testReadCachedLatestNews_whenLatestCache_shouldObserveNext(){
+
+    }
+
+    @Test
+    fun testReadCachedLatestNews_whenCacheIsOld_shouldObserveComplete(){
+
+    }
+
+    private fun prepareCacheFile(date : Int){
+        var fileContent = "{ date: \"${date}\", stories: [] }"
+
+    }
+
+    // delete the file, so the test is stateless
+    @After
+    fun cleanUp(){
+        // dir.path = "/var/folders/4w/1qcx1th56px_3vvfmbcy2zdh0000gp/T/robolectric695337270918799239/cache"
+        val dir = BaseApp.app.cacheDir
+        val file = File(dir, fileName)
+        file.delete()
+    }
+
 }
 
 /*
